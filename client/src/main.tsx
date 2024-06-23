69aad115f8609b4d3dc4f8d6dfe02e7df50b3717
pranjalpandey693 <pranjalpandey693@gmail.com>
2024-06-23 23:13:50 +0530

    client setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
      <ChakraProvider>
     <App />
-    ejfljfljflsdjflsjd
      </ChakraProvider>
   </React.StrictMode>,
 
