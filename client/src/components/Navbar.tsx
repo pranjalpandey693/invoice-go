f8d166cb13877e65a937b24ecf6da1b6ad16e487
pranjalpandey693 <pranjalpandey693@gmail.com>
2024-06-27 00:04:08 +0530

    rest

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { useColorMode } from "@chakra-ui/react";
+
+
+export default function Navbar(){
+
+    const { colorMode , toggleColorMode} = useColorMode()
+
+    return (
+        
+    )
+}
